4eb29b7736aede9df00785801cc105ec338e3463
farizwebdev <farizabar80@gmail.com>
2026-01-29 21:23:49 +0700

    Ubah codingan baru

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ProductDisplay />
+  </div>
+</template>
+
+<script>
+import ProductDisplay from './components/ProductDisplay.vue'
+import './assets/style/page.css' // Import CSS global di App.vue
+
+export default {
+  name: 'App',
+  components: {
+    ProductDisplay
+  }
+}
+</script>
